2ab76188b05d36ceedf34702ee87ddffd6622bff
vinay.singh <vinay.singh@KIIT_12194>
2021-07-28 03:18:08 +0530

    UI Improvement and Added the State Stats

--- src/Components/StatePage/StatePage.js
+++ src/Components/StatePage/StatePage.js
@@ -6,23 +6,37 @@ import { Line } from "react-chartjs-2";
 function StatePage(props) {
 	const stateName = props.match.params.stateName;
 	const loadedData = useSelector((state) => state.loadedData);
+
 	let districtArray = [],
 		confirmedArray = [],
 		recoveredArray = [],
-		deceasedArray = [];
+		deceasedArray = [],
+		confirmed = 0,
+		active = 0,
+		recovered = 0,
+		deceased = 0,
+		migratedother = 0;
+
 	if (loadedData ? loadedData[stateName] : null) {
 		districtArray = Object.keys(loadedData[stateName].districtData);
+
 		districtArray.map((district, index) => {
 			confirmedArray[index] =
 				loadedData[stateName].districtData[district].confirmed;
+
 			recoveredArray[index] =
 				loadedData[stateName].districtData[district].recovered;
+
 			deceasedArray[index] =
 				loadedData[stateName].districtData[district].deceased;
-			console.log();
+
+			recovered += loadedData[stateName].districtData[district].recovered;
+			deceased += loadedData[stateName].districtData[district].deceased;
+			confirmed += loadedData[stateName].districtData[district].confirmed;
+			active += loadedData[stateName].districtData[district].active;
+			migratedother += loadedData[stateName].districtData[district].migratedother;
 		});
 	}
-	// console.log(districtArray);
 
 	const config = {
 		labels: districtArray,
@@ -52,7 +66,14 @@ function StatePage(props) {
 	return (
 		<>
 			<h1>{props.match.params.stateName}</h1>
-			<Line data={config} />
+			<Line data={config} height='250px'/>
+			<div className='data-wrapper'>
+				<div className='Covid-stats'>Confirmed : {confirmed}</div>
+				<div className='Covid-stats'>Active : {active}</div>
+				<div className='Covid-stats'>Recovered : {recovered}</div>
+				<div className='Covid-stats'>Deceased : {deceased}</div>
+				<div className='Covid-stats'>Migrated to Other : {migratedother}</div>
+			</div>
 			<DistrictList data={districtArray} stateName={stateName} />
 		</>
 	);
