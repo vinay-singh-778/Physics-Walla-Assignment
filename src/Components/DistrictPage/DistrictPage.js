d1420e392ce820d5c39d59a910131c0b0906c19e
vinay.singh <vinay.singh@KIIT_12194>
2021-07-28 01:32:09 +0530

    finally data is showing in District Page and added a temporary chart in State Page

--- src/Components/DistrictPage/DistrictPage.js
+++ src/Components/DistrictPage/DistrictPage.js
@@ -1,10 +1,40 @@
+import React from "react";
+import "./DistrictPage.css";
+import { useSelector } from "react-redux";
+
 function DistrictPage(props) {
-    return (
-        <div>
-            {props.match.params.stateName}
-            {props.match.params.districtName}
-        </div>
-    )
+	const stateName = props.match.params.stateName;
+	const districtName = props.match.params.districtName;
+	const loadedData = useSelector((state) => state.loadedData);
+	let districtData = {
+       
+    };
+
+	if (loadedData ? loadedData[stateName] : null) {
+		districtData = loadedData[stateName]['districtData'][districtName];
+        // console.log(districtData);
+	}
+
+	return (
+		<>
+			<h3>District : {districtName}</h3>
+			<div className='data-wrapper'>
+				<div className='Covid-stats'>Recovered : {districtData.recovered}</div>
+				<div className='Covid-stats'>active : {districtData.active}</div>
+				<div className='Covid-stats'>confirmed : {districtData.confirmed}</div>
+				<div className='Covid-stats'>Migrated to Other : {districtData.migratedother}</div>
+				<div className='Covid-stats'>deceased : {districtData.deceased}</div>
+				<div>
+					<h7>Delta-Varient </h7>
+					<div className='Delta-stats'>
+						<div className='Covid-stats'>recovered : {districtData.delta.recovered}</div>
+						<div className='Covid-stats'>deceased: {districtData.delta.deceased}</div>
+						<div className='Covid-stats'>confirmed : {districtData.delta.confirmed}</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
 }
 
-export default DistrictPage
+export default DistrictPage;
--- src/Components/StatePage/StatePage.js
+++ src/Components/StatePage/StatePage.js
@@ -1,6 +1,7 @@
 import react, { useState, useEffect } from "react";
 import DistrictList from "../DistrictList/DistrictList";
 import { useSelector } from "react-redux";
+import { Line } from "react-chartjs-2";
 
 function StatePage(props) {
 	const stateName = props.match.params.stateName;
@@ -9,11 +10,39 @@ function StatePage(props) {
 	if (loadedData ? loadedData[stateName] : null) {
 		districtArray = Object.keys(loadedData[stateName].districtData);
 	}
-	console.log(districtArray);
+	// console.log(districtArray);
+
+	const config = {
+		labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun"],
+		datasets: [
+			{
+				label: "First dataset",
+				data: [33, 53, 85, 41, 44, 65],
+				fill: true,
+				backgroundColor: "rgba(75,192,192,0.2)",
+				borderColor: "rgba(75,192,192,1)",
+			},
+			{
+				label: "Second dataset",
+				data: [33, 25, 35, 51, 54, 76],
+				fill: false,
+				borderColor: "#742774",
+			},
+			{
+				label: "dataset",
+				data: [33, 25, 35, 51, 54, 76],
+				fill: false,
+				borderColor: "#742774",
+			},
+		],
+	};
 
 	return (
 		<>
 			<h1>{props.match.params.stateName}</h1>
+			<div className='graph-wrapper'>
+				<Line data={config} />
+			</div>
 			<DistrictList data={districtArray} stateName={stateName} />
 		</>
 	);
